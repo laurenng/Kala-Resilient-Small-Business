4d03fd7cf15de649ee16d9544e52ee5e46d27b17
AngelMcCrary-Lupson <angel.mccrary.lupson@gmail.com>
2021-04-28 11:20:35 -0700

    added redirects when logged in

--- kala-app/src/loginTest.tsx
+++ kala-app/src/loginTest.tsx
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import AppState from './reduxData/types';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import './userAuthentication.css';
 
 const emailRegex = "/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
 
 interface props {
-
+    isLogin: boolean
 }
 
 interface state {
-
+    isLogin: boolean
 }
 
 class LoginTest extends React.Component<props, state> {
@@ -20,7 +20,7 @@ class LoginTest extends React.Component<props, state> {
         super(props);
 
         this.state = {
-
+            isLogin: this.props.isLogin
         };
     } 
 
@@ -71,48 +71,58 @@ class LoginTest extends React.Component<props, state> {
                     console.log(data);
                     // add this to state here
                     // login was successful so redirect elsewhere
+                    sessionStorage.setItem('fundingToken', data.token);
+                    console.log(sessionStorage.getItem('fundingToken'));
+                    this.setState({"isLogin": true} as any); // need to hook up to actual state
                 }
             });
 
     }
 
     render() {
-        return(
-            <div id="loginBody">
-                <div id="loginPage">
-                    <h1>Log in</h1>
-                    <h3><a>If you are from Commerce, log in or sign up here</a></h3> {/* add href to commerece later */}
-                    <h3 id="loginErrMsg" className="hidden">There was a problem trying to login, please check your email or password and try again.</h3>
-                    <form id="loginForm" onSubmit={this.handleLogin}>
-                        <label>Email</label>
-                        <input id="userEmail" type="email" title="Please enter a valid email address. e.g. johnsmith@example.com" required></input>
-                        <label>Password</label>
-                        <input id="userPassword" type="password" required></input>
-                        <br></br>
-                        <button type="submit" id="loginBtn">Log In</button>
-                        <div id="lineBreak">
-                            <hr/>
-                            <p>OR</p>
-                            <hr/>
-                        </div>
-                        <button type="button" id="signupBtn"><Link to="/signup">Sign Up</Link></button>
-                    </form>
-                </div>
+        const isLogin = this.state.isLogin; // get redux state here
+        const loggedIn = (sessionStorage.getItem('fundingToken') !== null)
+        console.log("has just logged in " + isLogin + " | already logged in " + loggedIn)
+        const loginPage = 
+        <div id="loginBody">
+            <div id="loginPage">
+                <h1>Log in</h1>
+                <h3><a>If you are from Commerce, log in or sign up here</a></h3> {/* add href to commerece later */}
+                <h3 id="loginErrMsg" className="hidden">There was a problem trying to login, please check your email or password and try again.</h3>
+                <form id="loginForm" onSubmit={this.handleLogin}>
+                    <label>Email</label>
+                    <input id="userEmail" type="email" title="Please enter a valid email address. e.g. johnsmith@example.com" required></input>
+                    <label>Password</label>
+                    <input id="userPassword" type="password" required></input>
+                    <br></br>
+                    <button type="submit" id="loginBtn">Log In</button>
+                    <div id="lineBreak">
+                        <hr/>
+                        <p>OR</p>
+                        <hr/>
+                    </div>
+                    <button type="button" id="signupBtn"><Link to="/signup">Sign Up</Link></button>
+                </form>
             </div>
-        );
+        </div>
+
+        if (isLogin || loggedIn) {
+            return <Redirect to="/profile" />;
+        }
+        return loginPage;
     }
 
 }
 
 function mapStateToProps(state: AppState) {
     return { 
-        
+        // isLogin: state.isLogin
     }
 }
 
 function mapDispatchToProps(dispatch: any)  {
     return {
-        
+        // updateFilters: (  newFilters: Filters ) => dispatch(updateFilters(newFilters))
     }    
 } 
 
